eca0e5390dda94b0ccc63d4a800fb91a0d7ca6a4
Lamvo27072001 <lampham27072001@gmail.com>
2022-07-14 21:08:30 +0700

    2

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import TodoForm from './Components/TodoForm'
+import TodoList from './Components/TodoList'
+
+function App() {
+  return (
+    <TodoList/>
+    aaaaa
+  )
+}
+
+export default App
---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+import TodoForm from './TodoForm'
+
+function TodoList() {
+
+    const [todos, setTodos] = useState([])
+
+    const addTodo = todo => {
+        if(!todo.text || /^\s*$/.test(todo.text)) {
+          return; 
+        }
+    }
+  return (
+    <div className='todo-app'>
+        <h1>What's the Plan for Today?</h1>
+        <TodoForm/>
+    </div>
+  )
+}
+
+export default TodoList
